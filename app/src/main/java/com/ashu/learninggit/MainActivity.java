c71bd2915b1b786271bdb11de2f31d6e7ca5a6be
ashwini <https://ashwini123456@bitbucket.org/ashwini123456/samp-app.git>
2019-05-03 18:39:33 +0530

    commiting from cmd

---
+++ app/src/main/java/com/ashu/learninggit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ashu.learninggit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //super code begins here
+        //i don't know whats happening now
+    }
+}
